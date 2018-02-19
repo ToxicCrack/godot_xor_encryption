438bd3a87db3e849e88cb424f36e96035ec1e59a
Daniel <ToxicCrack@users.noreply.github.com>
2018-02-19 12:35:47 +0100

    Update encrypt.gd

--- encrypt.gd
+++ encrypt.gd
@@ -1,18 +1,15 @@
-func xor_encrypt(string, key=self.encryptKey):
+func xor_encrypt(string, key):
   var i = 0
   var newStr = []
   while (i < string.length()):
       newStr.append((string.to_utf8()[i] ^ key.to_utf8()[i % (key.length() - 1)]));
       i += 1;
-  i = 0;
-  print(JSON.print(newStr))
   return newStr;
   
-func xor_decrypt(enc, key=self.encryptKey):
+func xor_decrypt(enc, key):
   var output = PoolByteArray();
   var i = 0
   while (i < enc.size()):
       output.append((enc[i]) ^ key.to_utf8()[i % (key.length() - 1)]);
       i += 1
-  print(output.get_string_from_utf8())
   return output.get_string_from_utf8();
